5281a84858f239d1a02bfa4f4e13e9f1a1d54241
cindy <sunnycindyedwards@gmail.com>
2014-10-30 12:46:36 -0700

    update for heroku env vars

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# http://tammersaleh.com/posts/managing-heroku-environment-variables-for-local-development/
+# Load heroku vars from local file
+heroku_env = File.join(Rails.root, 'config', 'heroku_env.rb')
+load(heroku_env) if File.exists?(heroku_env)
+
 # Initialize the rails application
 Easyeval::Application.initialize!
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,18 +1,18 @@
-# Stripe.api_key = "sk_test_9apNfxhlmFafG3XVt2Kc0CmR"
+Stripe.api_key = "sk_test_9apNfxhlmFafG3XVt2Kc0CmR"
 
-# STRIPE_PUBLIC_KEY = "pk_test_Qe9EmkEmIvylZdQd1rWAWGsX"
+STRIPE_PUBLIC_KEY = "pk_test_Qe9EmkEmIvylZdQd1rWAWGsX"
 
 
 
 #http://stackoverflow.com/questions/12755867/rails-constant-not-initializing
 
-if Rails.env == 'production'
-  Stripe.api_key:    "productionapikeycode"
-  STRIPE_PUBLIC_KEY: "productionpublickeycode"
-else
-  Stripe.api_key:    "tesapikeycode"
-  STRIPE_PUBLIC_KEY: "testpublickeycode"     
-end 
+#if Rails.env == 'production'
+#  Stripe.api_key:    "productionapikeycode"
+#  STRIPE_PUBLIC_KEY: "productionpublickeycode"
+#else
+#  Stripe.api_key:    "tesapikeycode"
+#  STRIPE_PUBLIC_KEY: "testpublickeycode"     
+#end 
 
 
 StripeEvent.setup do
